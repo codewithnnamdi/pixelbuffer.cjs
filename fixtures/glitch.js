8568dfdfbed45212c26f0376d91e79d18f669c16
Nnamdi <okpalan86@gmail.com>
2023-02-06 14:40:46 +0000

    a

--- fixtures/glitch.js
+++ fixtures/glitch.js
@@ -1,3 +1,20 @@
 
+var canvas = document.getElementById('pixelbuffer');
+var ctx = canvas.getContext('2d');
+
+var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+var data = imageData.data;
+
+for (var i = 0; i < data.length; i += 4) {
+    if (Math.random() < 0.05) {
+        data[i] = Math.floor(Math.random() * 255);
+        data[i + 1] = Math.floor(Math.random() * 255);
+        data[i + 2] = Math.floor(Math.random() * 255);
+        data[i + 3] = 255
+    }
+
+
+}
+ctx.putImageData(imageData, 0, 0);
 
 
