c1509cce4997863d49798423a1df6296b6e3c30d
Nnamdi <okpalan86@gmail.com>
2023-02-06 13:03:32 +0000

    a

--- spec/pixelbuffer.spec.js
+++ spec/pixelbuffer.spec.js
@@ -12,12 +12,19 @@ describe('pixelbuffer', function () {
     expect(pb.getHeight()).to.equal(10);
   })
 
-  it("should have a data array", function () {
+  it("should have a data array with a length of 400", function () {
     expect(pb.getData()).to.be.an.instanceof(Uint8ClampedArray);
-  })
-
-  it("should have a data array of the correct size", function () {
     expect(pb.getData().length).to.equal(10 * 10 * 4);
   })
+
+  it("should have a clear method", function () {
+    expect(pb.clear).to.be.a('function');
+  })
+
+  it("should have a fill method", function () {
+    expect(pb.fill).to.be.a('function');
+  })
+
+  
 
 })
